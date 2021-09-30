da92b29ef735e483390dee23b9a3a7f45b1960f8
Francesco <francesco@zimmerman.team>
2021-09-30 16:23:52 +0200

    script to make a video of the evolving charts per-epoch

---
+++ plot_evolution_video.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PREFIX=$1
+if test -z "$PREFIX"; then
+    echo "first argument has to be a prefix"
+    exit
+fi
+
+ls $PREFIX* |
+    while read BEFORE; do
+        AFTER=$(echo $BEFORE | sed "s/^\([^0-9]*\)\([0-9]*\)_\(.*\)$/\1\2.png/g"); 
+        mv $BEFORE $AFTER; 
+    done
+
+ffmpeg \
+    -r:v 10 \
+    -i "$PREFIX_%04d.png" \
+    -codec:v libx264 \
+    -preset veryslow \
+    -pix_fmt yuv420p \
+    -crf 28 \
+    -an "$PREFIX.mp4"
