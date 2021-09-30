working on the text fields to fixed-lenght numerical values

diagnostics.py
@@ -4,14 +4,19 @@ import seaborn
 import tempfile
 import mlflow
 import utils
+import pandas as pd
 
 def barplots(npa,rel=None,type_=None): # FIXME duplicated code
-    assert type_ in ('fields','losses')
+    seaborn.set(rc={'figure.figsize': (27, 9)})
     if type_ == 'fields':
         assert rel is not None
         sections = rel.divide(npa)
     elif type_ == 'losses':
         sections = np.hsplit(npa, npa.shape[1])
+    elif type_ == 'latent':
+        sections = [npa]
+    else:
+        raise Exception("unknown type_ "+str(type_))
 
     vmin = utils.min_across(sections)
     vmax = utils.max_across(sections)
@@ -42,12 +47,16 @@ def barplots(npa,rel=None,type_=None): # FIXME duplicated code
     return fig
 
 def heatmaps(npa,rel=None,type_=None):
-    assert type_ in ('fields','losses')
+    seaborn.set(rc={'figure.figsize': (27, 9)})
     if type_ == 'fields':
         assert rel is not None
         sections = rel.divide(npa)
     elif type_ == 'losses':
         sections = np.hsplit(npa, npa.shape[1])
+    elif type_ == 'latent':
+        sections = [npa]
+    else:
+        raise Exception("unknown type_ "+str(type_))
 
     vmin = utils.min_across(sections)
     vmax = utils.max_across(sections)
@@ -75,6 +84,36 @@ def heatmaps(npa,rel=None,type_=None):
     fig.colorbar(axs[len(sections)-1].collections[0], cax=axs[len(sections)])
     return fig
 
+def correlation(data):
+    d = pd.DataFrame(data=data)
+    # Compute the correlation matrix
+    corr = d.corr()
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    corr_metric = np.mean(np.abs(np.array(corr.fillna(0))))
+    return corr, corr_metric, mask
+
+def correlation_heatmap(corr, corr_metric, mask, epoch_nr):
+    seaborn.set_theme(style="white")
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(10, 10))
+    # Generate a custom diverging colormap
+    cmap = seaborn.diverging_palette(230, 20, as_cmap=True)
+    # Draw the heatmap with the mask and correct aspect ratio
+    seaborn.heatmap(
+        corr,
+        mask=mask,
+        cmap=cmap,
+        vmin=-1,
+        vmax=1,
+        center=0,
+        square=True,
+        linewidths=.5,
+        cbar_kws={"shrink": .5}
+    )
+    ax.set(xlabel="epoch "+str(epoch_nr)+" avg abs corr="+str(corr_metric))
+    return fig
+
 def log_heatmaps_artifact(name, npa, which_tset,rel=None, type_=None):
     fig = heatmaps(npa, rel=rel, type_=type_)
     filename = tempfile.mktemp(prefix=f"heatmaps_{name}_{type_}_{which_tset}",suffix=".png")
@@ -87,4 +126,13 @@ def log_barplots_artifact(name, npa, which_tset,rel=None, type_=None):
     filename = tempfile.mktemp(prefix=f"barplots_{name}_{type_}_{which_tset}",suffix=".png")
     fig.savefig(filename)
     mlflow.log_artifact(filename)
+    return filename
+
+def log_correlation_heatmap_artifact(name, corr, corr_metric, mask, which_tset, epoch_nr):
+    print(f"creating and logging correlation heatmap for {name} {which_tset} epoch {epoch_nr}..")
+    fig = correlation_heatmap(corr, corr_metric, mask, epoch_nr)
+    filename = tempfile.mktemp(prefix=f"correlation_heatmap_{name}_{which_tset}_{epoch_nr:04}_",suffix=".png")
+    fig.savefig(filename)
+    mlflow.log_artifact(filename)
+    print("done creating and logging correlation heatmap")
     return filename

relspecs.py
@@ -4,12 +4,13 @@ import re
 from abc import abstractmethod
 import numpy as np
 import functools
-
+import sklearn.preprocessing
 import torch.nn
 
 import persistency
 import utils
-import sklearn.preprocessing
+import config
+import text_model
 
 @functools.cache
 def get_codelists():
@@ -175,7 +176,7 @@ class DatetimeField(AbstractField):
         # using only the first 3 values of the timetuple as they refer to Y/M/D
         x_hat_descaled = self.scaler.inverse_transform(x_hat)[:,:3]
         x_descaled = self.scaler.inverse_transform(x)[:,:3]
-        correct_ones = np.linalg.norm(x_hat_descaled - x_descaled,axis=1) > 0.5
+        correct_ones = np.linalg.norm(x_hat_descaled - x_descaled,axis=1) < 0.5
         correct_ratio = np.mean(correct_ones)
         return correct_ratio
 
@@ -186,11 +187,24 @@ class DatetimeField(AbstractField):
 class CategoryField(AbstractField):
     def __init__(self, name, codelist_name, **kwargs):
         if 'output_activation_function' not in kwargs:
-            kwargs['output_activation_function'] = torch.nn.Softmax()
-        if 'loss_function' not in kwargs:
-            kwargs['loss_function'] = utils.OneHotCrossEntropyLoss()
+            kwargs['output_activation_function'] = torch.nn.Softmax(dim=1)
+
+        prevent_constant_prediction = kwargs.pop('prevent_constant_prediction', None)
+
         super().__init__(name, **kwargs)
         self.codelist_name = codelist_name
+
+        if 'loss_function' not in kwargs:
+            if(prevent_constant_prediction):
+                prevent_constant_prediction_idx = self.codelist.index(prevent_constant_prediction)
+                siz = len(self.codelist)
+                weight = np.ones(siz)/siz
+                weight[prevent_constant_prediction_idx] /= 10
+                print('foo',weight[prevent_constant_prediction_idx])
+            else:
+                weight = None
+            kwargs['loss_function'] = utils.OneHotCrossEntropyLoss(weight=weight)
+
 
     @property
     def codelist(self):
@@ -250,8 +264,35 @@ class NumericalField(AbstractField):
         x_max = x*1.05
 
         n_correct = 0.0
-        for curr_x_hat, curr_x in zip(x_hat,x):
-            n_correct += float(x_hat[0] < curr_x_hat[0] and x_hat[0] > curr_x[0])
+        for curr_x_hat, curr_x_min, curr_x_max in zip(x_hat,x_min,x_max):
+            n_correct += float(curr_x_hat < curr_x_max and curr_x_hat > curr_x_min)
+        return n_correct/float(x.shape[0])
+
+class TextField(AbstractField):
+    def encode(self, entries, set_size, **kwargs):
+        ret = [text_model.instance.encode(x) for x in entries]
+        short_of = set_size - len(entries)
+        if short_of > 0:
+            for i in range(short_of):
+                ret.append(text_model.instance.empty_vector)
+        return ret
+
+    @property
+    def n_features(self):
+        return text_model.instance().n_features
+
+    def guess_correct(self, x_hat, x):
+        # within 10%
+        x_min = x*0.95
+        x_max = x*1.05
+
+        n_correct = 0.0
+        for curr_x_hat, curr_x_min, curr_x_max in zip(x_hat,x_min,x_max):
+            datapoint_correct = True
+            for x_hat_loc,x_min_loc,x_max_loc in zip(curr_x_hat,curr_x_min,curr_x_max):
+                datapoint_correct = datapoint_correct and x_hat_loc < x_max_loc
+                datapoint_correct = datapoint_correct and x_hat_loc > x_min_loc
+            n_correct += float(datapoint_correct)
         return n_correct/float(x.shape[0])
 
 rels = RelsCollection([
@@ -260,12 +301,19 @@ rels = RelsCollection([
             "value_currency",
             'Currency',
             #output_activation_function=torch.nn.Sigmoid(),
-            #loss_function=torch.nn.MSELoss()
+            #loss_function=torch.nn.MSELoss(),
+            prevent_constant_prediction='USD'
         ),
         CategoryField("type", 'BudgetType'),
         CategoryField("status", 'BudgetStatus'),
         DatetimeField("period_start_iso_date"),
         DatetimeField("period_end_iso_date"),
         NumericalField("value")
+    ]),
+    Rel("result",[
+        CategoryField("type", 'ResultType'),
+        TextField("aggregation_status", 'ResultAggregationStatus'),
+        TextField("title_narrative", 'ResultTitleNarrative'),
+        TextField("description_narrative", 'ResultDescriptionNarrative'),
     ])
 ])

simple_autoencoder.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 import numpy as np
 import mlflow
@@ -78,13 +80,16 @@ class AE(pl.LightningModule):
 
         self.kwargs = kwargs
 
-    def forward(self, features):
+    def encoder(self, features):
         activation = self.encoder_input_layer(features)
         activation = self.activation_function(activation)
         for curr in self.encoder_hidden_layers:
             activation = curr(activation)
             activation = self.activation_function(activation)
         code = self.encoder_output_layer(activation)
+        return code
+
+    def decoder(self, code):
         activation = self.decoder_input_layer(code)
         activation = self.activation_function(activation)
         for curr in self.decoder_hidden_layers:
@@ -97,6 +102,11 @@ class AE(pl.LightningModule):
             reconstructed.append(activation_out)
         reconstructed = self._glue(reconstructed)
         return reconstructed
+
+    def forward(self, features):
+        self.code = self.encoder(features)
+        self.reconstructed = self.decoder(self.code)
+        return self.reconstructed
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
@@ -118,7 +128,7 @@ class AE(pl.LightningModule):
     def _glue(self, tensor_list):
         return self.rel.glue(tensor_list)
 
-    def _loss(self,batch,x_hats):
+    def _loss(self,batch,x_hats,z):
         losses = []
         guess_correct = []
         batch_divided = self._divide(batch)
@@ -135,7 +145,11 @@ class AE(pl.LightningModule):
             curr_loss = loss_fn(curr_x_hat, batch_div)
             guess_correct.append(field.guess_correct(curr_x_hat.detach().numpy(), batch_div.detach().numpy()))
             losses.append(curr_loss)
+
         loss = functools.reduce(lambda a, b: a + b, losses)
+
+        latent_l1_norm = self.kwargs.pop('latent_l1_norm',0)
+        loss += torch.norm(z,p=1)*latent_l1_norm
         self.losses = [curr.detach().numpy() for curr in losses]
         self.guess_correct = guess_correct
         return loss
@@ -153,16 +167,18 @@ class AE(pl.LightningModule):
 
     def _step(self,batch,batch_idx,which_tset):
         x_hat_divided, x_hat_glued = self._divide_or_glue(self(batch))
+        z = self.encoder(batch)
         diff = batch - x_hat_glued
         mae = torch.mean(torch.abs(diff))
         mse = torch.mean((diff) ** 2)
-        loss = self._loss(batch,x_hat_divided)
+        loss = self._loss(batch,x_hat_divided,z)
         self.log(f"{which_tset}_loss", loss)
         self.log(f"{which_tset}_mae", mae)
         self.log(f"{which_tset}_mse", mse)
 
         self.diff = diff.detach().numpy()
         self.x_hat = x_hat_glued.detach().numpy()
+        self.z = z.detach().numpy()
         self.diff_reduced = np.mean(np.abs(self.diff), axis=0)
         return loss
 
@@ -184,7 +200,9 @@ class ValidationErrorAnalysisCallback(pl.callbacks.Callback):
     def _init_collected(self):
         for collection in (
                 'x_hat',
+                'z',
                 'output_last_epoch',
+                'latent_last_epoch',
                 # indexed by batch idx:
                 'diffs',
                 'diffs_reduced',
@@ -205,6 +223,7 @@ class ValidationErrorAnalysisCallback(pl.callbacks.Callback):
         self.collected['diffs_reduced'][which_tset].append(lm.diff_reduced)
         self.collected['diffs'][which_tset].append(lm.diff)
         self.collected['x_hat'][which_tset].append(lm.x_hat)
+        self.collected['z'][which_tset].append(lm.z)
         self.collected['losses'][which_tset].append(lm.losses)
         self.collected['guess_correct'][which_tset].append(lm.guess_correct)
 
@@ -214,7 +233,7 @@ class ValidationErrorAnalysisCallback(pl.callbacks.Callback):
     def on_validation_batch_end(self, _, lm, outputs, batch, batch_idx, dataloader_idx):
         self._collect(lm,'val')
 
-    def _epoch_end(self, which_tset, is_last_epoch=False):
+    def _epoch_end(self, which_tset, is_last_epoch=False, epoch_nr=None):
         diffs_reduced = np.vstack(self.collected['diffs_reduced'][which_tset])
         x_hats = np.vstack(self.collected['x_hat'][which_tset])
         stacked_losses = np.vstack(self.collected['losses'][which_tset])
@@ -233,20 +252,36 @@ class ValidationErrorAnalysisCallback(pl.callbacks.Callback):
         # empty the validation diffs, ready for next epoch
         self.collected['diffs_reduced'][which_tset] = []
         self.collected['diffs'][which_tset] = []
+        z = np.vstack(self.collected['z'][which_tset])
+
+        corr, corr_metric,mask = diagnostics.correlation(z)
+        mlflow.log_metric(f"{which_tset}_latent_corr_metric",corr_metric)
+        diagnostics.log_correlation_heatmap_artifact("latent",corr,corr_metric,mask,which_tset,epoch_nr)
         if is_last_epoch:
             ar = np.arange(x_hats.shape[0])
             rc = np.random.choice(ar,size=100)
             output_last_epoch = x_hats[rc,:]
+            latent_last_epoch = z[rc,:]
             self.collected['output_last_epoch'][which_tset] = output_last_epoch
+            self.collected['latent_last_epoch'][which_tset] = latent_last_epoch
         self.collected['x_hat'][which_tset] = []
+        self.collected['z'][which_tset] = []
         self.collected['losses'][which_tset] = []
         gc.collect()
 
     def on_train_epoch_end(self, trainer, lm):
-        self._epoch_end('train', is_last_epoch=lm.current_epoch==trainer.max_epochs-1)
+        self._epoch_end(
+            'train',
+            is_last_epoch=lm.current_epoch==trainer.max_epochs-1,
+            epoch_nr=lm.current_epoch
+        )
 
     def on_validation_epoch_end(self, trainer, lm):
-        self._epoch_end('val', is_last_epoch=lm.current_epoch==trainer.max_epochs-1)
+        self._epoch_end(
+            'val',
+            is_last_epoch=lm.current_epoch==trainer.max_epochs-1,
+            epoch_nr=lm.current_epoch
+        )
 
     def teardown(self, trainer, lm, stage=None):
         print("teardown stage", stage)
@@ -255,12 +290,16 @@ class ValidationErrorAnalysisCallback(pl.callbacks.Callback):
                 output_var_per_feature='fields',
                 output_mean_per_feature='fields',
                 mean_losses='losses',
+                mean_guess_correct='losses',
                 output_last_epoch='fields',
-                mean_guess_correct='losses'
+                latent_last_epoch='latent'
         ).items():
             for which_tset in self.collected[collected_name]:
                 curr = self.collected[collected_name][which_tset]
                 print(collected_name,which_tset)
+                if curr is None or len(curr)==0:
+                    logging.warning("f{collected_name} {which_tset} was empty")
+                    continue
                 if type(curr) is list:
                     stacked_npa = np.vstack(curr)
                 elif type(curr) is np.ndarray:
@@ -298,15 +337,16 @@ def main():
     mlflow.set_experiment("autoencoder_baseline")
     mlflow.pytorch.autolog()
     run_params = dict(
-        layers_width=196,
-        bottleneck_width=196,
+        layers_width=256,
+        bottleneck_width=32,
         activation_function="ELU",
         depth=2,
         weight_decay=1e-4,
-        max_epochs=500,
+        max_epochs=1000,
         rel_name='budget',
         batch_size=1024,
-        divide_output_layer=True
+        divide_output_layer=True,
+        latent_l1_norm=1.0
     ) # to yaml file?
     mlflow.log_params(run_params)
     mlflow.log_artifact(__file__)

utils.py
@@ -14,8 +14,13 @@ MONGODB_CONN="mongodb://mongouser:XGkS1wDyb4922@localhost:27017/learning_sets"
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class OneHotCrossEntropyLoss():
-    def __init__(self):
-        self.cross_entropy_fn = torch.nn.CrossEntropyLoss()
+    def __init__(self, weight = None):
+        #print("OneHostCrossEntropyLoss weight:",weight)
+        if weight is not None:
+            self.cross_entropy_fn = torch.nn.CrossEntropyLoss(weight=torch.Tensor(weight))
+        else:
+            self.cross_entropy_fn = torch.nn.CrossEntropyLoss()
+
     def __call__(self, x_hat, batch):
         labels = batch.argmax(1)
         ret = self.cross_entropy_fn(x_hat, labels)
@@ -83,13 +88,18 @@ def load_npa(filename):
         npa = deserialize(buf)
         return npa
 
-def load_npa_artifact(experiment_name, run_id,prefix="",suffix=""):
+def glob_artifacts(run_id,prefix="",suffix=""):
     run = mlflow.get_run(run_id)
     auri = run.info.artifact_uri
     apath = urllib.parse.urlparse(auri).path
     search = os.path.join(apath, prefix+"*"+suffix)
-    filename = glob.glob(search)[0]
-    npa = load_npa(filename)
+    found = glob.glob(search)
+    found = sorted(found)
+    return found
+
+def load_npa_artifact(experiment_name, run_id,prefix="",suffix=""):
+    found = glob_artifacts(run_id, prefix=prefix, suffix=suffix)
+    npa = load_npa(found[0])
     return npa
 
 def is_seq(stuff):