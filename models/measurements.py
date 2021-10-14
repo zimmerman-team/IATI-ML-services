b31055e9ce901e273aba82c6a24aecdfe0685c13
Francesco Stablum <francesco@zimmerman.team>
2021-10-14 17:59:40 +0200

    docs: documented measurements

--- models/measurements.py
+++ models/measurements.py
@@ -6,16 +6,29 @@ import logging
 import mlflow
 
 def log(*args):
+    """
+    module-wide logging function
+    """
     strs = map(str,args)
     msg = "measurements:"+" ".join(strs)
     logging.debug(msg)
 
 class PlotType(enum.Enum):
+    """
+    every Measurement may have one PlotType (plot_type).
+    This describes how the Measurement needs to be handled in the plots
+    """
     FIELDS = 'fields'
     LOSSES = 'losses'
     LATENT = 'latent'
 
 class MeasurementsCollection(utils.Collection):
+    """
+    Collection of Measurement type objects.
+    It allows to perform data collection from values and vectors temporarily
+    stored in a LightningModel, as well as to trigger aggregation operations
+    within the Collected Measurements.
+    """
     def collect(self, lm, which_tset, of_type):
         # measurements that have been utilized in the aggregations
         utilized = set()
@@ -47,6 +60,9 @@ class MeasurementsCollection(utils.Collection):
         gc.collect()
 
     def all_of_type(self,of_type):
+        """
+        returns all Measurements of the specified subclass
+        """
         ret = []
         for curr in self:
             if type(curr) is of_type:
@@ -54,6 +70,10 @@ class MeasurementsCollection(utils.Collection):
         return ret
 
     def src(self, name, of_type=None):
+        """
+        reverse of the `dst` field: returns all the names of the Measurements
+        that contribute to generate the given one (by name)
+        """
         ret = dict()
         for curr in self:
             if name in curr.dst_names:
@@ -73,32 +93,51 @@ class MeasurementsCollection(utils.Collection):
 
     @property
     def plottable(self):
+        """
+        :return: all Measurement objects that can be plotted
+                 (as specified in their constructor)
+        """
         ret = []
         for curr in self:
             if curr.plot_type is not None:
                 ret.append(curr)
         return ret
 
+# FIXME: namespace for aggregation functions?
 def mae(data):
+    """
+    Mean average error. An aggregation that can be set in the 'dst' parameter.
+    """
     return np.mean(np.abs(data),axis=0)
 
 def var(data):
+    """
+    Variance. An aggregation that can be set in the 'dst' parameter.
+    """
     return np.var(data,axis=0)
 
 def mean(data):
+    """
+    Mean. An aggregation that can be set in the 'dst' parameter.
+    """
     return np.mean(data, axis=0)
 
-def random_sampling(data):
+def random_sampling(data,amount=100):
     ar = np.arange(data.shape[0])
-    rc = np.random.choice(ar, size=100)
+    rc = np.random.choice(ar, size=amount)
     ret = data[rc,:]
     return ret
 
 class Measurement(object):
+    """
+    Every Measurement may be a data source for other Measurements which
+    are specified with the `dst` parameter, which effectively enables
+    the creation of a dependency graph.
+    """
     def __init__(
             self,
             name,
-            aggregation_fn=None,
+            aggregation_fn=None, # FIXME: delete if unused (legacy)
             dst=None,
             plot_type=None,
             mlflow_log=False
@@ -116,13 +155,24 @@ class Measurement(object):
             self.clear(which_tset.value)
 
     def clear(self,which_tset):
+        """
+        Some measures needs to be cleared, for example at the end of an epoch.
+        Example: collecting network output for the entire dataset.
+        Can easily clog the memory if it's not regularly emptied
+        """
         log(f"clearing {self.name} {which_tset}..")
         self.data[which_tset] = []
 
     def set(self, which_tset, data):
+        """
+        Replace entirely the data collected
+        """
         self.data[which_tset] = data
 
     def add(self, chunk, which_tset):
+        """
+        Adds some data to the collected
+        """
         if type(chunk) is np.ndarray:
             chunk_size = chunk.shape
         elif type(chunk) is list:
@@ -136,10 +186,16 @@ class Measurement(object):
         self.data[which_tset].append(chunk)
 
     def add_from_lm(self, lm, which_tset):
+        """
+        Collects a measurement from the LightningModel
+        """
         chunk = getattr(lm, self.name)
         self.add(chunk, which_tset)
 
     def vstack(self,which_tset):
+        """
+        Creates a numpy array from the data that has been collected
+        """
         data = self.data[which_tset]
         if len(data) == 0:
             return np.array([[]])
@@ -148,10 +204,17 @@ class Measurement(object):
 
     @property
     def dst_names(self):
+        """
+        returns the names of the measurements that make use of the data
+        collected in this Measurement
+        """
         return self.dst.keys() if self.dst else ()
 
     @property
     def dst(self):
+        """
+        :returns: Measurements that make use of the data collected in this Measurement
+        """
         return self._dst
 
     def __str__(self):
@@ -159,6 +222,9 @@ class Measurement(object):
         return f"<Measurement {self.name}>"
 
     def aggregate(self, which_tset, aggregation_fn):
+        """
+        Applies an aggregation function to the data collected in this Measurement
+        """
         stacked = self.vstack(which_tset)
         log(f"{self} aggregating with {aggregation_fn} on {which_tset} (shape {stacked.shape})..")
         ret = aggregation_fn(stacked)
@@ -166,12 +232,21 @@ class Measurement(object):
         return ret
 
     def aggregation(self, which_tset):
+        """
+        Applies the default aggregation function (FIXME: obsolete deprecated?)
+        """
         return self.aggregate(which_tset, self.aggregation_fn)
 
     def count(self, which_tset):
+        """
+        Count of measurements collected
+        """
         return len(self.data[which_tset])
 
     def _recursive_counts_str(self, stuff):
+        """
+        Helper function to print the sizes of the dimensions in the collected data
+        """
         if not utils.is_vectoriform(stuff):
             return "_"
         elif utils.is_empty(stuff):
