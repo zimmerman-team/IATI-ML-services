6f3e4702d67ccd4f260b820c94276e74489a5821
Francesco Stablum <francesco@zimmerman.team>
2021-10-18 17:43:42 +0200

    refactor: making it a bit easier to export mongo collections

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-MONGO_URI=$(bash mongo_uri.sh)
 VM_URI=$(bash vm_uri.sh)
-mongoexport --collection npas_tsets --out db_dumps/exported_npas_tsets.json $MONGO_URI
+bash mongo_dump.sh
 rsync -ruv --exclude mlruns .  $VM_URI:~/learning_sets/
