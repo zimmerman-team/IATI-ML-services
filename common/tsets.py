fix: fixed field scaling issue with set index

common/tsets.py
@@ -33,6 +33,7 @@ class Tsets(utils.Collection):
                     self[which_tset],
                     with_set_index=self.with_set_index
                 )
+                print("divided:",[s.shape for s in sections])
                 if self.with_set_index is True:
                     # enriches the Tsets object with train_set_index and test_set_index
                     # properties that contain a 1D-vector of set ids - positional, as
@@ -43,14 +44,22 @@ class Tsets(utils.Collection):
                 if which_tset == 'train':
                     # the preprocessing scaling functions are trained only
                     # on the training data, of course
-                    self._make_and_fit_scalers(sections)
+                    self._make_and_fit_scalers(sections, self.with_set_index)
 
                 # WARNING: this relies on the fact that the training
                 # set is going to be processed before the others
-                self[which_tset+"_scaled"] = self._scale(sections)
+                self[which_tset+"_scaled"] = self._scale(sections, self.with_set_index)
             else:
                 raise Exception(f"Didn't find {gridfs_filename}")
 
+    def print_shapes(self):
+        for which_tset in self.tsets_names:
+            print(which_tset+".shape",self[which_tset].shape)
+            print(which_tset+"_scaled.shape",self[which_tset+"_scaled"].shape)
+
+    @property
+    def item_dim(self):
+        return self.train.shape[1]
 
     def n_sets(self, which_tset):
         """
@@ -93,13 +102,16 @@ class Tsets(utils.Collection):
         ret.append([curr_start, all_count])
         return np.array(ret)
 
-    def _make_and_fit_scalers(self, sections):
+    def _make_and_fit_scalers(self, sections, with_set_index):
+        if with_set_index:
+            # but the index does not need to be scaled
+            sections = sections[1:]
         for field, section in zip(self.rel.fields, sections):
             # FIXME: is having the trained scaler in the field a good idea??
             field.make_and_fit_scaler(section)
 
 
-    def _scale(self, sections):
+    def _scale(self, sections, with_set_index):
         """
         Uses the previously-trained scalers to scale the given data,
         which is presented as a list of per-field tensors (sections)
@@ -108,9 +120,23 @@ class Tsets(utils.Collection):
         scaled tensors of each field.
         """
         scaled = []
+        if with_set_index:
+
+            # adding the index to the scaled quantities
+            scaled.append(sections[0])
+
+            # but the index does not need to be scaled
+            sections = sections[1:]
+
         for field, section in zip(self.rel.fields, sections):
+            assert field.n_features == section.shape[1], \
+                f"mismatch between field n_features and n columns of section: {field.n_features} != {section.shape[1]}"
             # FIXME: is having the trained scaler in the field a good idea??
+            print("scaling field",field,"section.shape:",section.shape)
             section_scaled = field.scaler.transform(section)
+            print('resulting section_scaled.shape:',section_scaled.shape)
             scaled.append(section_scaled)
+        print("scaled sections:",[s.shape for s in scaled])
+
         ret = self.rel.glue(scaled)
         return ret

models/dspn_autoencoder.py
@@ -38,14 +38,15 @@ class DSPNAE(generic_model.GenericModel):
 
         def __init__(self, data):
             self.data = data
+            print("CollateFn: data.shape",data.shape)
 
         def __call__(self, intervals):
             assert (len(intervals) == 1)
             interval = intervals[0] # because it's a batch of size 1
-            print("interval", interval)
+            print("CollateFn: interval", interval)
             start_item_index,end_item_index = interval[0:2]
             ret = torch.tensor(self.data[start_item_index:end_item_index])
-            print("ret.shape", ret.shape)
+            print("CollateFn: ret.shape", ret.shape)
             return ret
 
 
@@ -111,34 +112,37 @@ class DSPNAE(generic_model.GenericModel):
         self.decoder = dspn.dspn.DSPN(
             self.encoder,
             kwargs['item_dim'],
-            kwargs['max_set_size'],
+            self.max_set_size,
             kwargs['item_dim'],
-            10, # number of iteration on the decoder
-            800, # inner learning rate
+            10, # number of iteration on the decoder # FIXME: to conf
+            800, # inner learning rate # FIXME: to conf
         )
 
     def forward(self, loaded_set):
 
+        print("loaded_set size:",loaded_set.size())
         # loaded_set dimensionality: (set_size, item_dims)
         set_size = loaded_set.size(0)
         item_dims = loaded_set.size(1)
 
         # target_set dimensionality: (batch_size, item_dims, set_size)
         # here we assume a batch_size=1
-        target_set = torch.zeros(1,item_dims,set_size)
+        target_set = torch.zeros(1,item_dims,self.max_set_size)
         target_set[0,0:item_dims,0:set_size] = torch.swapaxes(loaded_set,0,1)
 
         # target_mask dimensionality: (batch_size, set_size)
-        target_mask = torch.zeros(1,item_dims,set_size)
-        target_set[0, 0:item_dims, 0:set_size] = 1
+        target_mask = torch.zeros(1,self.max_set_size)
+        target_set[0, 0:set_size] = 1
 
+        print('target_set.size()',target_set.size())
+        print('target_mask.size()',target_mask.size())
         self.code = self.encoder(target_set, target_mask)
         self.reconstructed = self.decoder(self.code)
         return self.reconstructed
 
     def _step(self, batch, batch_idx, which_tset):
         # copied from dspn.train.main.run()
-        (progress, masks, evals, gradn), (y_enc, y_label) = self( input, batch )
+        (progress, masks, evals, gradn), (y_enc, y_label) = self( batch )
 
         set_loss = dspn.utils.chamfer_loss(
             torch.stack(progress), batch.unsqueeze(0)

models/run.py
@@ -101,12 +101,14 @@ def run(Model,config_name):
         mlflow.log_artifact(__file__)
         mlflow.log_artifact(model_config['config_filename'])
         rel = relspecs.rels[model_config['rel_name']]
+        print("rel",rel)
 
         tsets = persistency.load_tsets(rel,with_set_index=Model.with_set_index)
 
         for curr in tsets.tsets_names:
             mlflow.log_param(f"{curr}_datapoints",tsets[curr].shape[0])
-        item_dim = tsets.train.shape[1]
+        item_dim = tsets.item_dim
+        print("item_dim is",item_dim)
 
         #  use gpu if available
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -119,6 +121,7 @@ def run(Model,config_name):
             **model_config
         ).to(device)
 
+        tsets.print_shapes()
         train_loader = model.make_train_loader(tsets)
         test_loader = model.make_test_loader(tsets)
 