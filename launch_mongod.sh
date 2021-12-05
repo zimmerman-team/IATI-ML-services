73c0951903051123dd9de572cc9be2fe6d615453
Francesco Stablum <francesco@zimmerman.team>
2021-12-05 23:20:52 +0100

    feat: mongod launch script

---
+++ launch_mongod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#FIXME: duplicated code with launch_mlflow_ui.sh
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo SCRIPT_DIR: $SCRIPT_DIR
+LEARNING_SETS_DIR="$(readlink -f $SCRIPT_DIR/..)"
+echo LEARNING_SETS_DIR: $LEARNING_SETS_DIR
+
+cd $LEARNING_SETS_DIR
+
+TS=$(date +%Y%m%d_%H%M%S)
+
+screen -X -S mongod quit
+
+sleep 1
+
+screen -L -Logfile logs/mongod_${TS}.log -S mongod -d -m mongod --config /etc/mongod.conf
+
+echo "done."
+
