0a1f37d37aaea6ee01b5d2778025e5b328d4c829
Francesco Stablum <francesco@zimmerman.team>
2021-12-16 01:18:48 +0100

    fix: dashboard proper refresh times

--- system_dashboard.sh
+++ system_dashboard.sh
@@ -6,9 +6,9 @@ tmux new-window "watch -t 'df -h'" \; \
   split-window -f -v "watch -t 'free -h'" \; \
   split-window -f -v "watch -t 'screen -ls'" \; \
   split-window -v "watch -t 'ps aux | grep airflow'" \; \
-  split-window -f -h "watch -t 'bash mongodb_diagnostics.sh'" \; \
+  split-window -f -h "watch -n 30 -t 'bash mongodb_diagnostics.sh'" \; \
   split-window -v "watch -n 10 -t 'python3 airflow/print_running_tasks.py'" \; \
   split-window -f -v "htop" \; \
-  split-window -h "watch -t 'python3 npas_tsets_summary.py'" \;
+  split-window -h "watch -n 30 -t 'python3 npas_tsets_summary.py'" \;
 ) &
 tmux
