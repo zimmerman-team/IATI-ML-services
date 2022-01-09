4ce384f21db2899de024d05b62e38833414448c6
Francesco Stablum <francesco@zimmerman.team>
2022-01-09 12:25:49 +0100

    fix: making sure statsd_exporter is definitely terminated upon relaunch

--- monitoring/launch_statsd_exporter.sh
+++ monitoring/launch_statsd_exporter.sh
@@ -8,6 +8,8 @@ echo LEARNING_SETS_DIR: $LEARNING_SETS_DIR
 TS=$(date +%Y%m%d_%H%M%S)
 
 screen -X -S statsd_exporter quit
+sleep 1
+killall statsd_exporter
 
 cd ${LEARNING_SETS_DIR}/monitoring
 screen -L -Logfile ${LEARNING_SETS_DIR}/logs/statsd_exporter_${TS}.log -S statsd_exporter -d -m ${LEARNING_SETS_DIR}/monitoring/statsd_exporter/statsd_exporter --statsd.mapping-config=${LEARNING_SETS_DIR}/monitoring/statsd_exporter_mapping.conf
