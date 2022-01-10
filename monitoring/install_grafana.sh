af689a4e773d6e5305782b7ac04e3446d407f408
Francesco Stablum <francesco@zimmerman.team>
2022-01-10 12:51:49 +0100

    fix: limiting services access to localhost

--- monitoring/install_grafana.sh
+++ monitoring/install_grafana.sh
@@ -7,6 +7,11 @@ wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
 sudo apt-get update
-sudo apt-get install grafana
+sudo apt-get -y install grafana crudini etckeeper
+
+crudini --set /etc/grafana/grafana.ini server http_port 3000
+
+# limit service access to localhost
+crudini --set /etc/grafana/grafana.ini server http_addr 127.0.0.1
 
 sudo systemctl enable grafana-server.service
--- monitoring/launch_grafana.sh
+++ monitoring/launch_grafana.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo systemctl daemon-reload
+sudo systemctl stop grafana-server
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
