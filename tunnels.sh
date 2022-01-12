205e25693498132a7e862960cf4088ecf577f6c1
Francesco Stablum <francesco@zimmerman.team>
2022-01-12 19:02:13 +0100

    fix: tunnels script dynamic in vm uri

--- tunnels.sh
+++ tunnels.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-ssh -L 8081:127.0.0.1:8080 root@ml.nyuki.io -N -f
-ssh -L 9091:127.0.0.1:9090 root@ml.nyuki.io -N -f
-ssh -L 19125:127.0.0.1:9125 root@ml.nyuki.io -N -f
-ssh -L 19102:127.0.0.1:9102 root@ml.nyuki.io -N -f
-ssh -L 3001:127.0.0.1:3000 root@ml.nyuki.io -N -f
+VM_URI=$(bash config/vm_uri.sh)
+
+ssh -L 8081:127.0.0.1:8080 $VM_URI -N -f
+ssh -L 9091:127.0.0.1:9090 $VM_URI -N -f
+ssh -L 19125:127.0.0.1:9125 $VM_URI -N -f
+ssh -L 19102:127.0.0.1:9102 $VM_URI -N -f
+ssh -L 3001:127.0.0.1:3000 $VM_URI -N -f
+ssh -L 18888:127.0.0.1:8888 $VM_URI -N -f
