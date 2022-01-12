8508d53eb2b9366b09cbbf21c3e54d5e9da465c6
Francesco Stablum <francesco@zimmerman.team>
2022-01-12 19:01:39 +0100

    fix: codelists now defaulting to empty lists; loss_function attribute becomes factory

--- airflow/install_local_airflow.sh
+++ airflow/install_local_airflow.sh
@@ -13,6 +13,29 @@ echo SCRIPT_DIR: $SCRIPT_DIR
 LEARNING_SETS_DIR="$(readlink -f $SCRIPT_DIR/..)"
 echo LEARNING_SETS_DIR: $LEARNING_SETS_DIR
 
+apt install -y m4 postgresql python3-pip libpq-dev gnupg
+
+# FIXME: to requirement.txt
+pip install torchvision ipython hiddenlayer matplotlib seaborn apache-airflow psycopg2 numpy sklearn pymongo torch mlflow gensim nltk pytorch_lightning
+
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+#UBUNTU_CODENAME=$(lsb_release -dc | grep Codename | awk '{print $2}')
+UBUNTU_CODENAME=focal
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $UBUNTU_CODENAME/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+apt update
+sudo apt-get install -y mongodb-org
+service mongod start
+
+MONGO_USER="$(bash $LEARNING_SETS_DIR/config/get_conf_item.sh mongo_user)"
+MONGO_PASSWORD="$(bash $LEARNING_SETS_DIR/config/get_conf_item.sh mongo_password)"
+
+mongosh <<- EOM
+use learning_sets;
+db.dropUser("$MONGO_USER");
+db.createUser({ user: "$MONGO_USER", pwd: "$MONGO_PASSWORD", roles: [ { role: "readWrite", db: "soccer" }] });
+
+EOM
+
 # FIXME: automate conf item extraction?
 AIRFLOW_PG_PASSWORD="$(bash $LEARNING_SETS_DIR/config/get_conf_item.sh airflow_pg_password)"
 AIRFLOW_USER="$(bash $LEARNING_SETS_DIR/config/get_conf_item.sh airflow_user)"
