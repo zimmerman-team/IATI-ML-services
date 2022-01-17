d7e271dab85135607b4506b2f543c0e2fc37a132
Francesco Stablum <francesco@zimmerman.team>
2022-01-17 10:40:24 +0100

    fix: missing timestamps in statsd log file

--- monitoring/launch_statsd.sh
+++ monitoring/launch_statsd.sh
@@ -5,6 +5,8 @@ echo SCRIPT_DIR: $SCRIPT_DIR
 LEARNING_SETS_DIR="$(readlink -f $SCRIPT_DIR/..)"
 echo LEARNING_SETS_DIR: $LEARNING_SETS_DIR
 
+TS=$(date +%Y%m%d_%H%M%S)
+
 screen -X -S statsd quit
 
 screen -L -Logfile $LEARNING_SETS_DIR/logs/statsd_${TS}.log -S statsd -d -m node statsd/stats.js statsdConfig.js
