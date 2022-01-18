685f959270f63b39f6edb9d1545ac5fe161d2f11
Francesco Stablum <francesco@zimmerman.team>
2022-01-18 11:21:13 +0100

    feat: test dags with tqdm

--- preprocess/test_dag.py
+++ preprocess/test_dag.py
@@ -2,6 +2,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import datetime
+import tqdm
+import time
 
 default_args = {
     'retries': 2,
@@ -15,8 +17,16 @@ def test_task(ti):
     :param ti:
     :return:
     """
-    return True
+    pass
 
+def tqdm_sleep_task(ti):
+    for i in tqdm.tqdm(range(3600)):
+        time.sleep(1)
+
+def tqdm_sleep_print_task(ti):
+    for i in tqdm.tqdm(range(3600)):
+        time.sleep(1)
+        print(i)
 
 with DAG(
         'aaa_test_dag__',
@@ -27,6 +37,20 @@ with DAG(
     t_test = PythonOperator(
         task_id='test_task',
         python_callable=test_task,
+        start_date=days_ago(2),
+        op_kwargs={}
+    )
+
+    t_tqdm_sleep = PythonOperator(
+        task_id='tqdm_sleep_task',
+        python_callable=tqdm_sleep_task,
+        start_date=days_ago(2),
+        op_kwargs={}
+    )
+
+    t_tqdm_sleep_print = PythonOperator(
+        task_id='tqdm_sleep_print_task',
+        python_callable=tqdm_sleep_print_task,
         start_date=days_ago(2),
         op_kwargs={}
     )
