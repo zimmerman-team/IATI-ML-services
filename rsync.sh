d3ebd231e3023774c1aa82109005192a72f05455
Francesco Stablum <francesco@zimmerman.team>
2022-01-18 11:27:40 +0100

    fix: rsync exclude *.pyc

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VM_URI=$(bash config/vm_uri.sh)
 #bash mongo_dump.sh
-rsync -ruv --exclude .git --exclude 'mlruns*' --exclude logs --exclude corpora --exclude db_dumps .  $VM_URI:~/learning_sets/
+rsync -ruv --exclude '*.pyc' --exclude .git --exclude 'mlruns*' --exclude logs --exclude corpora --exclude db_dumps .  $VM_URI:~/learning_sets/
