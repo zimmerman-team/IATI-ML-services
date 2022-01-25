ac5fbcf601ff647f435cc12e10da13536950a3dd
Francesco Stablum <francesco@zimmerman.team>
2022-01-25 09:23:05 +0100

    feat: allowing using same ports for tunnelled services by different local ip

--- tunnels.sh
+++ tunnels.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# we use an alternative local ip address to represent the remote machine
+TUNNEL_IP=127.0.2.1
+
+# also we add an alternative hostname instead of "localhost" and we call this "tunnel"
+TUNNEL_HOSTNAME=tunnel
+
+# lists of ports on the remote machine that are going to be mapped locally
+PORTS="8080 9090 9125 9102 3000 5000 8888"
+
 VM_URI=$(bash config/vm_uri.sh)
 
-ssh -L 8081:127.0.0.1:8080 $VM_URI -N -f
-ssh -L 9091:127.0.0.1:9090 $VM_URI -N -f
-ssh -L 19125:127.0.0.1:9125 $VM_URI -N -f
-ssh -L 19102:127.0.0.1:9102 $VM_URI -N -f
-ssh -L 3001:127.0.0.1:3000 $VM_URI -N -f
-ssh -L 5001:127.0.0.1:5000 $VM_URI -N -f
-ssh -L 18888:127.0.0.1:8888 $VM_URI -N -f
+sudo cp /etc/hosts /etc/hosts.bck
+
+# adding the tunnel hostname and ip address to /etc/hosts only if not already there
+sudo grep -q "$TUNNEL_HOSTNAME" /etc/hosts || printf "$TUNNEL_IP $TUNNEL_HOSTNAME\n" | sudo tee -a /etc/hosts
+
+for CURR in $PORTS ; do
+  echo "current port: $CURR . Can be accessed via ${TUNNEL_HOSTNAME}:${CURR}"
+  ssh -L ${TUNNEL_HOSTNAME}:${CURR}:localhost:${CURR} $VM_URI -N -f
+done
