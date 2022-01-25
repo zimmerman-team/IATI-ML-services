7cbc140312696cff803697fab801a520ccde3db5
Francesco Stablum <francesco@zimmerman.team>
2022-01-25 09:20:47 +0100

    feat: very quick model training tasks for debugging purposes

--- models/dag.py
+++ models/dag.py
@@ -7,16 +7,13 @@ import datetime
 import os
 import sys
 
-import models.dspn_autoencoder
-
 project_root_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__))+"/..")
 sys.path = [project_root_path]+sys.path
 
+import models.dspn_autoencoder
+
 from common import relspecs, config
 from models import run
-
-config_name = config.models_dag_config_name
-
 
 def in_days(n):
     """
@@ -27,7 +24,7 @@ def in_days(n):
     return today + datetime.timedelta(days=n)
 
 
-def train_model(_rel, ti):
+def train_model(_rel, config_name, ti):
     """
     task function to train a Deep Set Prediction Network
     on a specific relation.
@@ -53,35 +50,40 @@ default_args = {
     'retry_delay': datetime.timedelta(minutes=5),
     'schedule_interval': None
 }
-with DAG(
-        'train_dspn_models',
-        description='trains DSPN models',
-        tags=['train', 'dspn', 'sets', 'models'],
-        default_args=default_args,
-        schedule_interval=None,
-        concurrency=1,  # maximum two models being trained at the same time
-        max_active_runs=1,
-        max_active_tasks=1
-) as dag:
-    days_interval = config.models_dag_days_interval
-    for rel_i, rel in enumerate(relspecs.rels):
+def make_dag(dag_name, config_name):
+    with DAG(
+            dag_name,
+            description='trains DSPN models',
+            tags=['train', 'dspn', 'sets', 'models'],
+            default_args=default_args,
+            schedule_interval=None,
+            concurrency=1,  # maximum two models being trained at the same time
+            max_active_runs=1,
+            max_active_tasks=1
+    ) as dag:
+        days_interval = config.models_dag_days_interval
+        for rel_i, rel in enumerate(relspecs.rels):
 
-        # this time the tasks are shell commands
-        train_cmd = f"cd {project_root_dir}; python3 models/dspn_autoencoder.py"\
-                    + f" {config.models_dag_config_name} --rel_name={rel.name}"
+            # this time the tasks are shell commands
+            train_cmd = f"cd {project_root_dir}; python3 models/dspn_autoencoder.py"\
+                        + f" {config_name} --rel_name={rel.name}"
 
-        t_train_model = BashOperator(
-            task_id=f"train_dspn_model_{rel.name}",
-            depends_on_past=False,
-            bash_command=train_cmd,
-            start_date=days_ago(2),
-            dag=dag
-        )
+            t_train_model = BashOperator(
+                task_id=f"train_dspn_model_{rel.name}",
+                depends_on_past=False,
+                bash_command=train_cmd,
+                start_date=days_ago(2),
+                dag=dag
+            )
 
         # PythonOperator version:
         # t_train_model = PythonOperator(
         #    task_id=f"train_dsp_model_{rel.name}",
         #    python_callable=train_model,
         #    start_date=in_days((rel_i-1)*days_interval),
-        #    op_kwargs={'rel':rel}
+        #    op_kwargs={'rel':rel,'config_name':config_name}
         # )
+    return dag
+
+dag_train_models = make_dag('train_models_dag',config.models_dag_config_name)
+dag_train_models_test = make_dag('train_models_dag_test',config.models_dag_config_name_test)
