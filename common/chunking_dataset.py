ca65bbdb2eb756d54321838b5b2c1a86fd170536
Francesco Stablum <francesco@zimmerman.team>
2022-02-03 10:02:56 +0100

    feat: chunking dataset is shuffling the chunks themselves at every dataset sweep,  and not only the datapoints within a chunk

--- common/chunking_dataset.py
+++ common/chunking_dataset.py
@@ -4,25 +4,71 @@ import torch
 import math
 import random
 import logging
+import mlflow
 
 class ChunkingDataset(torch.utils.data.IterableDataset):
     """
     Enables splitting the entire dataset sweep into multiple epochs
     """
 
-    def __init__(self, data, shuffle=False, subset_len=None):
+    def __init__(self, data, shuffle=False, chunk_len=None, log_mlflow=False):
         """
         :param data: list-like dataset
         :param shuffle: by using this dataset type, the shuffling has to be done within the dataset instead of the data loader
-        :param subset_len: length of each chunk
+        :param chunk_len: length of each chunk
         """
-        logging.debug(f"ChunkingDataset __init__ shuffle {shuffle}, subset_len {subset_len}")
+        logging.debug(f"ChunkingDataset __init__ shuffle {shuffle}, chunk_len {chunk_len}")
+        self.log_mlflow = log_mlflow
         self.all_data = list(data) # FIXME: maybe an iterator implementation is memory saving
-        self.n_calls = 0 # used to calculate the current epoch's dataset chunk's indexes
+
+        # used to calculate the current epoch's dataset chunk's indexes
+        # it's a constantly increasing index of how many items __iter__ is being called
+        self.n_calls = 0
+
         self.shuffle = shuffle # shuffles the datapoints within the chunk
-        assert subset_len is not None
-        self.subset_len = int(subset_len) # forcing int cast because cmdline arg may enter as string
-        self.subset = []
+        assert chunk_len is not None
+        self.chunk_len = int(chunk_len) # forcing int cast because cmdline arg may enter as string
+        self.chunk = []
+        self._create_slicing_indexes()
+        self.log_param("chunking_dataset_shuffle",self.shuffle)
+        self.log_param("chunking_dataset_chunk_len",self.chunk_len)
+        self.log_param("chunking_dataset_all_data_len",self.all_data_len)
+        self.log_param("chunking_dataset_n_chunks",self.n_chunks)
+
+    def _create_slicing_indexes(self):
+        self._slicing_indexes = []
+        for chunk_index in range(self.n_chunks):
+            datapoint_start = chunk_index * self.chunk_len
+            datapoint_end = (chunk_index + 1) * self.chunk_len
+            self._slicing_indexes.append((datapoint_start, datapoint_end))
+
+    def log_param(self,name,val):
+        """
+        param logging wrapper around mlflow
+        """
+        if self.log_mlflow:
+            mlflow.log_param(name,val)
+
+    def log_metric(self,name,val):
+        """
+        metric logging wrapper around mlflow
+        """
+        if self.log_mlflow:
+            mlflow.log_metric(name,val)
+
+    @property
+    def all_data_len(self):
+        """
+        :return: amount of datapoints
+        """
+        return len(self.all_data)
+
+    @property
+    def n_chunks(self):
+        """
+        :return: amount of chunks
+        """
+        return int(math.ceil(self.all_data_len / self.chunk_len))
 
     def __iter__(self):
         """
@@ -31,22 +77,28 @@ class ChunkingDataset(torch.utils.data.IterableDataset):
         shuffle the dataset, and this needs to be performed within the dataset (see shuffle init parameter)
         :return:
         """
-        all_data_len = len(self.all_data)
-        logging.debug(f"ChunkingDataset __iter__ id {id(self)}, all_data_len {all_data_len}, subset_len {self.subset_len}")
+        logging.debug(f"ChunkingDataset __iter__ id {id(self)}, all_data_len {self.all_data_len}, chunk_len {self.chunk_len}")
 
-        n_starts = int(math.ceil(all_data_len/self.subset_len))
-        logging.debug(f"ChunkingDataset __iter__ n_starts {n_starts}, n_calls {self.n_calls}")
-        section_index = self.n_calls % n_starts
-        logging.debug(f"ChunkingDataset __iter__ section_index {section_index}")
-        datapoint_start = section_index * self.subset_len
-        datapoint_end = (section_index+1) * self.subset_len
+        logging.debug(f"ChunkingDataset __iter__ n_chunks {self.n_chunks}, n_calls {self.n_calls}")
+        self.log_metric("chunking_dataset_n_calls",self.n_calls)
+        chunk_index = self.n_calls % self.n_chunks
+        if chunk_index == 0:
+            # shuffle the which chunks to select at every entire dataset sweep
+            random.shuffle(self._slicing_indexes)
+        self.log_metric("chunking_dataset_chunk_index",chunk_index)
+        logging.debug(f"ChunkingDataset __iter__ chunk_index {chunk_index}")
+        datapoint_start, datapoint_end = self._slicing_indexes[chunk_index]
+        self.log_metric("chunking_dataset_datapoint_start",datapoint_start)
+        self.log_metric("chunking_dataset_datapoint_end",datapoint_end)
+        progress = float(chunk_index)/float(self.n_chunks)
+        self.log_metric("chunking_dataset_progress",progress)
 
         logging.debug(f"ChunkingDataset __iter__ , datapoint_start {datapoint_start}, datapoint_end {datapoint_end}")
-        self.subset = self.all_data[datapoint_start:datapoint_end]
+        self.chunk = self.all_data[datapoint_start:datapoint_end]
         if self.shuffle:
-            random.shuffle(self.subset)
+            random.shuffle(self.chunk)
         self.n_calls += 1
-        return iter(self.subset)
+        return iter(self.chunk)
 
 def _command_line_test():
     """
@@ -55,9 +107,9 @@ def _command_line_test():
     """
     logging.basicConfig( level=logging.DEBUG )
     def make_dataset():
-        # a dataset of size 30 and a subset_len of 15 are enough to show how
+        # a dataset of size 30 and a chunk_len of 15 are enough to show how
         # ChunkingDataset splits the dataset sweeps into two epochs
-        dataset = ChunkingDataset(range(30), shuffle=True, subset_len=15)
+        dataset = ChunkingDataset(range(30), shuffle=True, chunk_len=15)
         return dataset
 
     def run_epochs(X):
