c591c263663160ef78dc20f5590b36d4ef835bb0
Francesco Stablum <francesco@zimmerman.team>
2022-02-17 08:52:26 +0100

    fix: exclude trained_models from rsync

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VM_URI=$(bash config/vm_uri.sh)
 #bash mongo_dump.sh
-rsync -ruv --exclude '*.pyc' --exclude .git --exclude 'mlruns*' --exclude logs --exclude corpora --exclude db_dumps --exclude 'prometheus*.tar.gz.*' --exclude 'mlflow_prometheus' .  $VM_URI:~/learning_sets/
+rsync -ruv --exclude '*.pyc' --exclude .git --exclude 'mlruns*' --exclude logs --exclude corpora --exclude db_dumps --exclude 'prometheus*.tar.gz.*' --exclude 'mlflow_prometheus' --exclude 'trained_models' .  $VM_URI:~/learning_sets/
 
