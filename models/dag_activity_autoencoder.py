1261b30f76a6a02f2971ecb3dad7631a901a3e3d
Francesco Stablum <francesco@zimmerman.team>
2022-03-10 11:18:26 +0100

    feat: model introspection for dag_activity_autoncoder

--- models/dag_activity_autoencoder.py
+++ models/dag_activity_autoencoder.py
@@ -9,17 +9,11 @@ project_root_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__))+"
 sys.path = [project_root_path]+sys.path
 
 import models
+from models import introspection
 
 import models.dspn_autoencoder
 
 from common import relspecs, config
-
-def all_modelnames():
-    ret = []
-    for curr in dir(models):
-        if hasattr(getattr(models, curr), 'Model'):
-            ret.append(curr)
-    return ret
 
 
 project_root_dir = os.path.abspath(os.path.join(
@@ -45,9 +39,6 @@ def make_dag(dag_name, config_name, modelname):
             max_active_tasks=config.models_dag_training_tasks_concurrency
     ) as dag:
 
-        # FIXME: clarify the purpos of this variable
-        days_interval = config.models_dag_days_interval
-
         # this time the tasks are shell commands
         train_cmd = f"cd {project_root_dir}; python3 models/{modelname}.py"\
                     + f" {config_name}"
@@ -63,6 +54,6 @@ def make_dag(dag_name, config_name, modelname):
     return dag
 
 thismodule = sys.modules[__name__]
-for modelname in all_modelnames():
+for modelname in introspection.all_modelnames():
     train_models_dag = make_dag(f"train_models_dag_{modelname}",config.models_dag_config_name, modelname)
     setattr(thismodule, "train_models_dag_"+modelname, train_models_dag)
