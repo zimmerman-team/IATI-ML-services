bf18ca73831a1031a015873d7469c40753398e16
Francesco Stablum <francesco@zimmerman.team>
2022-04-07 09:45:58 +0200

    refactor: multi-line split for long launching command with many parameters

--- monitoring/launch_statsd_exporter.sh
+++ monitoring/launch_statsd_exporter.sh
@@ -12,4 +12,10 @@ sleep 1
 killall statsd_exporter
 
 cd ${LEARNING_SETS_DIR}/monitoring
-screen -L -Logfile ${LEARNING_SETS_DIR}/logs/statsd_exporter_${TS}.log -S statsd_exporter -d -m ${LEARNING_SETS_DIR}/monitoring/statsd_exporter/statsd_exporter --statsd.mapping-config=${LEARNING_SETS_DIR}/monitoring/statsd_exporter_mapping.conf --web.listen-address="127.0.0.1:9102" --statsd.listen-udp="127.0.0.1:9125" --statsd.listen-tcp="127.0.0.1:9125"
+screen -L -Logfile ${LEARNING_SETS_DIR}/logs/statsd_exporter_${TS}.log \
+  -S statsd_exporter -d \
+  -m ${LEARNING_SETS_DIR}/monitoring/statsd_exporter/statsd_exporter \
+  --web.listen-address="127.0.0.1:9102" \
+  --statsd.listen-udp="127.0.0.1:9125" \
+  --statsd.listen-tcp="127.0.0.1:9125" \
+  --statsd.mapping-config=${LEARNING_SETS_DIR}/monitoring/statsd_exporter_mapping.conf
