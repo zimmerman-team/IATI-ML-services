9a47aa6c5ff17908e3db41d5bb51ec60949e56ec
Francesco Stablum <francesco@zimmerman.team>
2022-04-14 11:10:40 +0200

    feat: airflow webserver launch script

--- airflow/launch_airflow_webserver.sh
+++ airflow/launch_airflow_webserver.sh
@@ -10,16 +10,14 @@ cd $LEARNING_SETS_DIR
 
 TS=$(date +%Y%m%d_%H%M%S)
 
-screen -X -S airflow_scheduler quit
 screen -X -S airflow_webserver quit
 
 sleep 1
 
-ps aux | grep airflow | grep -v launch | awk '{print $2}' | xargs kill
+ps aux | grep airflow | grep webserver | grep -v launch | awk '{print $2}' | xargs kill
 
 sleep 1
 
-screen -L -Logfile logs/airflow_scheduler_${TS}.log -S airflow_scheduler -d -m airflow scheduler
 screen -L -Logfile logs/airflow_webserver_${TS}.log -S airflow_webserver -d -m airflow webserver -H 127.0.0.1
 
 echo "done."
