0d9bfba04738164a23735593f8389614d06078e1
Francesco Stablum <francesco@zimmerman.team>
2022-04-14 16:02:16 +0200

    feat: dockers monitoring tool

---
+++ services/cadvisor/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=v0.44.0
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=127.0.4.1:8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  gcr.io/cadvisor/cadvisor:$VERSION
