23646623a41e4743f2997299632ae860e7a6b323
Francesco Stablum <francesco@zimmerman.team>
2022-05-25 09:58:17 +0200

    fix: correct listening ip address for cadvisor

--- services/cadvisor/run.sh
+++ services/cadvisor/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 VERSION=v0.44.0
+
+# remove previous instance
+sudo docker stop /cadvisor
+sudo docker rm /cadvisor
+
 sudo docker run \
   --volume=/:/rootfs:ro \
   --volume=/var/run:/var/run:ro \
   --volume=/sys:/sys:ro \
   --volume=/var/lib/docker/:/var/lib/docker:ro \
   --volume=/dev/disk/:/dev/disk:ro \
-  --publish=127.0.4.1:18080:8080 \
+  --publish=127.0.3.1:18080:8080 \
   --detach=true \
   --name=cadvisor \
   --privileged \
