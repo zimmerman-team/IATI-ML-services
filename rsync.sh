489b87835d5aa99b833561496bdb99e812389126
Francesco Stablum <francesco@zimmerman.team>
2022-05-25 09:52:43 +0200

    fix: excluding directories from rsync, specifically docker volumes

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VM_URI=$(bash config/vm_uri.sh)
 #bash mongo_dump.sh
-rsync -ruv --exclude '*.pyc' --exclude .git --exclude 'mlruns*' --exclude logs --exclude corpora --exclude db_dumps --exclude 'prometheus*.tar.gz.*' --exclude 'mlflow_prometheus' --exclude 'trained_models' --exclude 'trained_models.old' .  $VM_URI:~/learning_sets/
+rsync -ruv --exclude services/airflow_scheduler/code --exclude mongo/db --exclude postgresql_for_airflow/volume/data --exclude '*.pyc' --exclude .git --exclude 'mlruns*' --exclude logs --exclude corpora --exclude db_dumps --exclude 'prometheus*.tar.gz.*' --exclude 'mlflow_prometheus' --exclude 'trained_models' --exclude 'trained_models.old' .  $VM_URI:~/learning_sets/
 
